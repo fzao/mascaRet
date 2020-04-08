36977a1f63c3734e51d0e7b87478b8eefb28f432
Fabrice Zaoui <fabrice.zaoui@edf.fr>
2020-04-08 19:17:56 +0200

    add a vignette for tutorial

---
+++ vignettes/steady_example.R
@@ -0,0 +1,10 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+## ----inter--------------------------------------------------------------------
+library(mascaRet)
+
+## ----loading------------------------------------------------------------------
+fpath <- system.file("extdata", "mascaret.files", package="mascaRet")
+print(fpath)
+
